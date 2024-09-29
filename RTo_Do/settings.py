631b5d01b3045fec24014fde3bdfe0ffe78c41ff
rayhan <156523736+TheRayhan009@users.noreply.github.com>
2024-09-29 20:06:25 +0600

    Update settings.py

--- RTo_Do/settings.py
+++ RTo_Do/settings.py
@@ -124,4 +124,4 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 EMAIL_HOST_USER = 'therayhan009@gmail.com'
-EMAIL_HOST_PASSWORD = 'gewhwibgfrjwltsi'
+EMAIL_HOST_PASSWORD = 'zhwidotutipstzdm'
